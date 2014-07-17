b20f227d871be1c464e71fd6caff2ff0188a42bf
duke <werdnx@gmail.com>
2014-07-17 16:13:49 +0400

    Initial commit

---
+++ join.py
@@ -0,0 +1,27 @@
+import MapReduce
+import sys
+
+
+mr = MapReduce.MapReduce()
+
+
+def mapper(record):
+    key = record[1]
+    value = record
+    mr.emit_intermediate(key, value)
+
+
+def reducer(key, list_of_values):
+    order = None
+    for v in list_of_values:
+        if v[0] == "order":
+            order = v
+
+    for v in list_of_values:
+        if v[0] != "order":
+            mr.emit(order + v)
+
+
+if __name__ == '__main__':
+    inputdata = open(sys.argv[1])
+    mr.execute(inputdata, mapper, reducer)
